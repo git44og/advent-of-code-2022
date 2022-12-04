2ebc8dc54f8a373ccc40b3f25ae3406201ff859d
Jens Fischer <>
2022-12-04 10:09:47 +0100

    04

---
+++ 04/01.py
@@ -0,0 +1,29 @@
+def getRangeCodes(line):
+    return line.strip().split(',')
+
+def getRange(rangeLine):
+    start, end = rangeLine.split('-')
+    return range(int(start), int(end)+1)
+
+def getContains(range1, range2):
+    for i in range1:
+        if i not in range2:
+            return False
+    return True
+
+with open('04/01 task.txt') as f:
+    lines = f.readlines()
+
+score = 0
+for line in lines:
+    rangeCode = getRangeCodes(line)
+    pairRange = [
+        getRange(rangeCode[0]),
+        getRange(rangeCode[1]),
+    ]
+    if getContains(pairRange[0], pairRange[1]) or getContains(pairRange[1], pairRange[0]):
+        score += 1
+
+    
+    
+print (score)
